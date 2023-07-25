1b7a76586e70779cd689c6e669b6ff9da7403cfb
hrushidighe <hrushikeshdighe1999@gmail.com>
2023-07-25 08:45:28 +0530

    largest number

---
+++ Numbers/src/LargestNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		try(Scanner sc=new Scanner(System.in)) {
+			
+			System.out.println("enter the 1st number");
+			int a=sc.nextInt();
+			System.out.println("enter the 2nd number");
+			int b=sc.nextInt();
+			System.out.println("enter the 2nd number");
+			int c=sc.nextInt();
+			
+			if(a>b &&a>c) {
+				System.out.println("the largest number is ="+a);
+			}
+			else if(b>a &b>c)
+				System.out.println("the largest number is ="+b);
+			else
+				System.out.println("the largest number is ="+c);
+		}
+
+	}
+
+}
